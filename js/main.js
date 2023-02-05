a49225ece1e80c802e8f90cb88c89c926065459f
Moe Nakajima <muskbergamot@gmail.com>
2023-02-05 15:56:23 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function clickbutton(target){
+
+    let result = document.getElementById("result")
+    console.log(target.innerHTML);
+    let target_value = target.innerHTML;
+    
+    if (target_value == "AC"){
+       
+        result.innerHTML = "0"
+    
+    }else if(target_value == "="){
+
+        result.innerHTML = eval(result.innerHTML)
+        
+    }else{
+        if(result.innerHTML == "0"){
+            result.innerHTML = target_value
+        }else{
+            result.innerHTML = result.innerHTML + target_value
+        }
+}
+}
